1ee0f98b2c7f1649bcdc9c2c719295ae3fcbd8ca
silvia-fl <68796944+silvia-fl@users.noreply.github.com>
2020-08-24 05:14:09 +0200

    Update scrip_guide.js

--- scrip_guide.js
+++ scrip_guide.js
@@ -1,26 +1,23 @@
-// Author: Nayra de Silvia
-
-
 // LocalStorage System
 
     // This fuction is called when a checkbox is clicked
     $('.CIS0').on('click', function() {
-        var fav, favs = []; // array
-        $('.CIS0').each(function() { // run through each of the checkboxes
-          fav = {id: $(this).attr('id'), value: $(this).prop('checked')};
-          favs.push(fav);
-        });
-        localStorage.setItem("CIS0", JSON.stringify(favs)); // Save new array
+      var fav, favs = []; // array
+      $('.CIS0').each(function() { // run through each of the checkboxes
+        fav = {id: $(this).attr('id'), value: $(this).prop('checked')};
+        favs.push(fav);
       });
-      
-      // This fuction is called when the page is uploaded
-      $(document).ready(function() {
-        var CIS0 = JSON.parse(localStorage.getItem('CIS0'));
-        if (!CIS0.length) {return};
-        console.debug(CIS-1);
-      
-        for (var i=0; i<CIS0.length; i++) {
-          console.debug(CIS0[i].value == 'on');
-          $('#' + CIS0[i].id ).prop('checked', CIS0[i].value);
-        }
-      });
+      localStorage.setItem("CIS0", JSON.stringify(favs)); // Save new array
+    });
+    
+    // This fuction is called when the page is uploaded
+    $(document).ready(function() {
+      var CIS0 = JSON.parse(localStorage.getItem('CIS0'));
+      if (!CIS0.length) {return};
+      console.debug(CIS0);
+    
+      for (var i=0; i<CIS0.length; i++) {
+        console.debug(CIS0[i].value == 'on');
+        $('#' + CIS0[i].id ).prop('checked', CIS0[i].value);
+      }
+    });
